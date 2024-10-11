0b12510728d2ce550fbfc8e52b7f2603bca4a8cd
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-10-11 15:36:56 +0300

    init

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/xuri/excelize/v2"
@@ -18,18 +19,108 @@ type SearchResult struct {
 	Content  string `json:"content"`
 }
 
+type FileIndex struct {
+	Filename    string    `json:"filename"`
+	LastUpdated time.Time `json:"last_updated"`
+}
+
+// Global değişken olarak dosya indekslerini saklama
+var fileIndexMap = make(map[string]FileIndex)
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("XLSX Metin Arama Uygulamasına Hoş Geldiniz!")
+		return c.SendString("XLSX Metin Arama ve İndeksleme Uygulamasına Hoş Geldiniz!")
 	})
 
+	// Tüm dosyaları indeksleme endpoint'i
+	app.Get("/index", func(c *fiber.Ctx) error {
+		indexXLSXFiles("./xlsx_files")
+		return c.JSON(fiber.Map{
+			"message": "Tüm dosyalar indekslendi",
+			"files":   fileIndexMap,
+		})
+	})
+
+	// Yeni dosyaları kontrol eden endpoint
+	app.Get("/check-new", func(c *fiber.Ctx) error {
+		newFiles := checkForNewFiles("./xlsx_files")
+		return c.JSON(fiber.Map{
+			"message":  "Yeni dosyalar kontrol edildi",
+			"newFiles": newFiles,
+		})
+	})
+
+	// Arama endpoint'i
 	app.Get("/search", searchXLSX)
 
 	log.Fatal(app.Listen(":3000"))
 }
 
+// Dizindeki tüm .xlsx dosyalarını indeksleyen fonksiyon
+func indexXLSXFiles(directory string) {
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("Dosya erişim hatası %s: %v", path, err)
+			return nil
+		}
+		if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".xlsx") {
+			fileIndexMap[info.Name()] = FileIndex{
+				Filename:    info.Name(),
+				LastUpdated: info.ModTime(),
+			}
+			log.Printf("Dosya indekslendi: %s", info.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		log.Printf("Dizin taranırken hata oluştu: %v", err)
+	}
+}
+
+// Yeni dosyaları kontrol eden fonksiyon
+func checkForNewFiles(directory string) []FileIndex {
+	var newFiles []FileIndex
+
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("Dosya erişim hatası %s: %v", path, err)
+			return nil
+		}
+		if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".xlsx") {
+			// Dosya zaten indekslenmiş mi kontrol et
+			if indexedFile, exists := fileIndexMap[info.Name()]; exists {
+				// Eğer dosya değişmişse, güncelle ve listeye ekle
+				if info.ModTime().After(indexedFile.LastUpdated) {
+					log.Printf("Dosya güncellendi: %s", info.Name())
+					fileIndexMap[info.Name()] = FileIndex{
+						Filename:    info.Name(),
+						LastUpdated: info.ModTime(),
+					}
+					newFiles = append(newFiles, fileIndexMap[info.Name()])
+				}
+			} else {
+				// Yeni dosya bulunursa, indekse ekle ve listeye ekle
+				log.Printf("Yeni dosya bulundu: %s", info.Name())
+				fileIndexMap[info.Name()] = FileIndex{
+					Filename:    info.Name(),
+					LastUpdated: info.ModTime(),
+				}
+				newFiles = append(newFiles, fileIndexMap[info.Name()])
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Dizin taranırken hata oluştu: %v", err)
+	}
+
+	return newFiles
+}
+
+// XLSX dosyaları içinde arama yapan mevcut fonksiyon
 func searchXLSX(c *fiber.Ctx) error {
 	searchText := c.Query("text")
 	if searchText == "" {
@@ -76,6 +167,7 @@ func searchXLSX(c *fiber.Ctx) error {
 	return c.JSON(results)
 }
 
+// Bir dosyada arama yapma fonksiyonu
 func searchInFile(filePath, searchText string) ([]SearchResult, error) {
 	f, err := excelize.OpenFile(filePath)
 	if err != nil {
